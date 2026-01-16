97cefa0f191ca2af109b67ec4f0de9799580b62e
yhos <guayabala565@gmail.com>
2026-01-16 11:45:16 -0600

    Practica02 - Ejercicio03
    add: se agrego el codigo del ejercicio 03: funciones que aun no se ha conectado

---
+++ js/ejercicio03.js
@@ -0,0 +1,85 @@
+//personalizaciuon el separador del ejercicio 03
+console.log("%cEjercicio 03: Funciones", style_console);
+//1. funciones Proocedurares, funciones vacias (VOID) o funciones que no retornan un valor
+function saludar()
+{
+    console.log("Bienvenido, al sistema de Bienes raices");
+}
+//console.log(saludar());
+console.warn("1.Funciones procedurales");
+saludar();
+//2 crear funciiones procedurales con parametros de entrada , es decir que reciben datos para funcionar
+function saludarUsuario(nombreUsuario, generarUsuario)
+{
+    if(generarUsuario="H")
+        console.log(`Bienvenido ${nombreUsuario} al sistema de Bienes Raices`);
+    else if (generarUsuario="M") console.log(`Bienvenida ${nombreUsuario} al sistema de Bienes Raices`); 
+    else
+        console.log(`Bienvenid@ ${nombreUsuario} al sistema de Bienes Raices`);
+}
+console.warn("2.Funciones Procedurales con parametros de emntrada");
+
+console.log("Nombre usuario= yhios genero=H ")
+saludarUsuario("Yhos", "H");
+console.log("Nombre usuario= Estela genero=M ")
+saludarUsuario("Estela", "M");
+console.log("Nombre usuario= JAN  genero=null ")
+saludarUsuario("JAN", null);
+//3.funciones que retornan un valor pero que no reciben parametros (datos de entrada)
+
+function fechaActual(){
+const fecha=new Date();
+const dias=["Domingo","Lunes","MArtes","Miercoles","Jueves","Viernes","Sabado"];
+const meses= [
+  "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio",
+  "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"
+];
+const fecha_formatoMX=`${dias[fecha.getDay()]},${fecha.getDate()} de ${meses[fecha.getMonth()]} de
+ ${fecha.getFullYear()}`;
+return fecha_formatoMX
+}
+console.warn("3. funciones que retornan un valor , pero no tienen parametros de entrada")
+let hoy=fechaActual();
+console.log (fechaActual());
+console.log(typeof(fechaActual));
+console.log(hoy);
+
+//funcioones  que retornan un valor y reciben parametros (datos de entrada )
+function login(username, password)
+{
+    let userValidation=false;
+    if (username=="admin" && password=="12345"){
+        userValidation=true;
+        console.log("Bienvenido usuario admin");
+    }
+    else if (username==="admin" && password!="12345"){
+        userValidation=false;
+        console.log("error en la contraseña del admin");
+    }
+    else if (username==="admin" && password=="12345"){
+        userValidation=false;
+        console.log("error el usuario no existe");
+    }
+    else if (username!="admin" && password!="12345"){
+        userValidation=false;
+        console.log("Porfavor verifica todo");
+} return userValidation;
+}
+console.warn("4. funciones que retornan un dato y reciben parametros de entrada")
+var loginStatus;
+//test 1- usuario y contraseña  correctos
+console.log("test1-usuario:admin, password:12345")
+loginStatus=login("admin","12345");
+console.log(`${loginStatus? "el usuario admin se ha logueado correctamente ":"hubo un error en el login del usuario admin."}`);
+
+console.log("test2-usuario:admin, password:1234567890")
+loginStatus=login("admin","1234567890");
+console.log(`${loginStatus? "el usuario admin se ha logueado correctamente ":"hubo un error en el login del usuario admin."}`);
+
+console.log("test3-usuario:admin, password:12345")
+loginStatus=login("administrador","12345");
+console.log(`${loginStatus? "el usuario admin se ha logueado correctamente ":"hubo un error en el login del usuario admin."}`);
+
+console.log("test4-usuario:marco, password:abcd")
+loginStatus=login("marco","abcd");
+console.log(`${loginStatus? "el usuario admin se ha logueado correctamente ":"hubo un error en el login del usuario admin."}`);
